65e3a09d7ab08fb2b9b4afae73c0b399b1687464
Jorge <jlcalleu18@gmail.com>
2021-04-24 18:54:27 -0400

    comments deleted

--- homework_8/script.js
+++ homework_8/script.js
@@ -7,66 +7,6 @@ function runJQuery() {
         },
     });
 }
-// function mapCollege() {
-//     //read selection
-//     let selectOption = $("#collegeList").val();
-//     // console.log(selectOption);
-//     // alert(selectOption);
-//   //extraction individual values from string selectOption
-//     let id = selectOption.split(",")[0];
-//     let collegetype = selectOption.split(",")[1];
-//     let college = selectOption.split(",")[2];
-//     let website = selectOption.split(",")[3];
-//     let address = selectOption.split(",")[4];
-//     let city = selectOption.split(",")[5];
-//     let state = selectOption.split(",")[6];
-//     let zipcode = selectOption.split(",")[7];
-//     let latitude = selectOption.split(",")[8];
-//     let longitude = selectOption.split(",")[9];
-//     let phone = selectOption.split(",")[10];
-
-//     console.log(typeof id);
-
-//     const myLatLng = {
-//         lat: 40.7128,
-//         lng: -74.0060,
-//     };
-//     map = new google.maps.Map(document.getElementById("showmap"), {
-//         zoom: 15.5,
-//         center: myLatLng,
-
-//     });
-//     const contentString =
-//         '<div id="content">' +
-//         "<p>"+
-//         "<b>"+college +"</b>"+ 
-//         "<br />" + address + "<br />" +
-//         city + ", " +state + ", " + zipcode +
-//         "<br />" + phone + "<br />" +
-//         "</p>" +
-//         "<p><b>Website: </b><a href=" + website + ">" + website + "</a></p>";
-//         "</div>";
-//     const infowindow = new google.maps.InfoWindow({
-//         content: contentString,
-//     });
-
-//     const marker = new google.maps.Marker({
-//         label: id,
-//         position: myLatLng,
-//         map,
-//         title: "id",
-//     });
-
-//     marker.addListener("click", () => {
-//         infowindow.open(map, marker);
-//     });
-//     var circle = new google.maps.Circle({
-//         map: map,
-//         radius: 16093,    // 10 miles in metres
-//         fillColor: '#AA0000'
-//       });
-//       circle.bindTo('center', marker, 'position');
-// }
 
 function initMap() {
 
@@ -142,12 +82,3 @@ function geocodeAddress(geocoder, resultsMap) {
     });
 }
 initMap();
-
-
-
-
-
-//console.log();
-// var lat = values[1][8];
-// var long = values[1][8];
-//console.log(values.length);
