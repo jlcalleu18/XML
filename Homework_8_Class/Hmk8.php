b30da62126598a4182b0351e3601724e65a92200
Jorge <jlcalleu18@gmail.com>
2021-04-26 18:02:21 -0400

    Adding homework 8 + my work

--- Homework_8_Class/Hmk8.php
+++ Homework_8_Class/Hmk8.php
@@ -6,9 +6,11 @@
 <body>
 <?php
     //get passing variables: address and distance
-    $addressIn = $_GET["addressKey"];
+    $latIn = $_GET["latKey"];
+    $longIn = $_GET["longKey"];
     $distanceIn = $_GET["distanceKey"];
 
+    //print "Lat ".$latIn." long: ". $longIn." distance: ". $distanceIn;
     //connect to database: collegesmap_db
     //set up 4 parameters
     $server = "localhost";
@@ -24,19 +26,19 @@
     //print"connected";
 
     //create a string variable that holds the SQL command
-    $SQLselect = "SELECT * FROM " . $databaseTable;
+    //$SQLselect = "SELECT * FROM " . $databaseTable;
+
+    $SQLselect = "SELECT *, ( 3959 * acos( cos( radians(".$latIn.") ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(".$longIn.") ) + sin( radians(".$latIn.") ) * sin(radians(latitude)) ) ) AS distance FROM collegesmap_table HAVING distance < ".$distanceIn." ORDER BY distance ";
     
     //to run the above SQL command = PHP has a funtion: mysqli_query()
     //store the results of the run in a variable
     $results = mysqli_query($mycon, $SQLselect) or die(" query did not run");
-    
+    // 40.695507 -73.987882
 
     //is there any records 
     $numrecs = mysqli_num_rows($results);
 
     if ($numrecs > 0) {
-        print "<select id='collegeList' onchange='mapCollege()'>";
-        print "<option value=''>Select a College...</option>";
 
         //loop through the matching record(s)
         while ($recordArray = mysqli_fetch_row($results)) {
@@ -53,16 +55,43 @@
             $latitude = $recordArray[8];
             $longitude = $recordArray[9];
             $phone = $recordArray[10];
+            $distance = $recordArray[11];
 
             //passing Variable
-            $passingInfo = $id.",".$colletype.",".$college.",".$website.",".$address.",".$city.",".$state.",".$zipcode.",".$latitude.",".$longitude.",".$phone;
-
-            print "<option value='".$passingInfo."'>$college</option>";        
+            // $passingInfo = $id.",".$colletype.",".$college.",".$website.",".$address.",".$city.",".$state.",".$zipcode.",".$latitude.",".$longitude.",".$phone.",".$distanceIn;
+            $return_arr[] = array("id" => $id,
+            "colletype" => $colletype,
+            "college" => $college,
+            "website" => $website,
+            "address" => $address,
+            "city" => $city,
+            "state" => $state,
+            "zipcode" => $zipcode,
+            "latitude" => $latitude,
+            "longitude" => $longitude,
+            "phone" => $phone,
+            "distance" => $distanceIn,
+        );
+                
     }
-        print "</select>";
+echo json_encode($return_arr);
     }else {
         print "No record(s) found";
     }
+
+    // echo json_encode ($id);
+    // echo json_encode ($colletype);
+    // echo json_encode ($college);
+    // echo json_encode ($website);
+    // echo json_encode ($address);
+    // echo json_encode ($city);
+    // echo json_encode ($state);
+    // echo json_encode ($zipcode);
+    // echo json_encode ($latitude);
+    // echo json_encode ($longitude);
+    // echo json_encode ($phone);
+    // echo json_encode ($distanceIn);
 ?>    
+
 </body>
 </html>
