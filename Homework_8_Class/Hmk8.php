37aba0bbd744f736d60483c32c93b08832330aa6
Jorge <jlcalleu18@gmail.com>
2021-04-28 12:23:18 -0400

    class 04-28-2021

--- Homework_8_Class/Hmk8.php
+++ Homework_8_Class/Hmk8.php
@@ -4,14 +4,9 @@
     $latIn = $_GET["latKey"];
     $longIn = $_GET["longKey"];
     $distanceIn = $_GET["distanceKey"];
-    // $latIn = $_POST['lati'];
-    // $longIn = $_POST['long'];
-    // $distanceIn = $_POST['inputDistance'];
 
-    print "latitude: ".$latIn." longitude: ".$longIn." Distance: ".$distanceIn;
-    //$data = array($latIn, $longIn, $distanceIn);
-    // $data = array("lat" => $latIn, "long" => $longIn, "dist" =>$distanceIn);
-   // echo json_encode($data);
+
+    //print "latitude: ".$latIn." longitude: ".$longIn." Distance: ".$distanceIn;
 
     //connect to database: collegesmap_db
     //set up 4 parameters
@@ -30,7 +25,7 @@
     //create a string variable that holds the SQL command
     //$SQLselect = "SELECT * FROM " . $databaseTable;
 
-    $SQLselect = "SELECT *, ( 3959 * acos( cos( radians(".$latIn.") ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(".$longIn.") ) + sin( radians(".$latIn.") ) * sin(radians(latitude)) ) ) AS distance FROM collegesmap_table HAVING distance < ".$distanceIn." ORDER BY distance ";
+    $SQLselect = "select *, (3959 * acos(sin(radians(" . $latIn . ")) * sin(radians(latitude)) + "."cos(radians(" . $latIn . ")) * cos(radians(latitude)) * cos(radians(longitude) - "."radians(" . $longIn . ")))) as distance from " . $databaseTable." having distance < ".$distanceIn;
     
     //to run the above SQL command = PHP has a funtion: mysqli_query()
     //store the results of the run in a variable
@@ -39,12 +34,14 @@
 
     //is there any records 
     $numrecs = mysqli_num_rows($results);
-
+    $allmatchingcolleges = "";
+    $index = 0;
     if ($numrecs > 0) {
 
         //loop through the matching record(s)
         while ($recordArray = mysqli_fetch_row($results)) {
 
+            $index++;
             //extracting field's values
             $id = $recordArray[0];
             $colletype = $recordArray[1];
@@ -59,10 +56,19 @@
             $phone = $recordArray[10];
             $distance = $recordArray[11];
 
-            $return_data = array($id, $colletype, $college, $website, $address, $city, $state, $zipcode, $latitude, $longitude, $phone, $distanceIn,);
+            // $return_data = array($id, $colletype, $college, $website, $address, $city, $state, $zipcode, $latitude, $longitude, $phone, $distanceIn,);
+            $passingInfo = $id.",".$colletype.",".$college.",".$website.",".$address.",".$city.",".$state.",".$zipcode.",".$latitude.",".$longitude.",".$phone;
+
+            //ensure last matching record does not have a ";"at the end
+            if ($index < $numrecs) {
+                $allmatchingcolleges .= $passingInfo.";";
+            }else {
+                $allmatchingcolleges .= $passingInfo;
+            }
                 
     }
-    // echo json_encode($return_data);
+    // send all matching colleges
+    print $allmatchingcolleges;
 
     }else {
         print "No record(s) found";
