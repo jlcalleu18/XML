0a3111ca870d421919d2a3b568ce3cee18fda5b3
Jorge <jlcalleu18@gmail.com>
2021-05-03 14:51:34 -0400

    final version, class work

--- Homework_9/stock.php
+++ Homework_9/stock.php
@@ -2,7 +2,8 @@
 //getting input search
 $searchText = $_GET["searchKey"];
 print"User's search: ".$searchText;
-    //connect to database: natbkawards_db
+print " ";
+    //connect to database: stock_db
     //set up 4 parameters
     $server = "localhost";
     $user = "root";
@@ -18,10 +19,6 @@ print"User's search: ".$searchText;
 
     //create a string variable that holds the SQL command
     $SQLselect = "SELECT * FROM " . $databaseTable." WHERE MATCH(exchange,symbol,name,lastSale,marketCap,ADRTSO,IPOyear,sector,industry,summaryQuote) "."AGAINST ('".$searchText."' IN NATURAL LANGUAGE MODE)";
-    
-    //$SQLselect = "select authorImage from " . $databaseTable;
-    
-
 
     //to run the above SQL command = PHP has a funtion: mysqli_query()
     //store the results of the run in a variable
@@ -33,27 +30,17 @@ print"User's search: ".$searchText;
     
     if ($numrecs > 0) {
 
-        print "<table border = '1'>";
-        
-        print "<tr>";
-            print"<th>exchange</th>";
-            print"<th>symbol</th>";
-            print"<th>name</th>";
-            print"<th>lastSale</th>";
-            print"<th>marketCap</th>";
-            print"<th>ADRTSO</th>";
-            print"<th>IPOyear</th>";
-            print"<th>sector</th>";
-            print"<th>industry</th>";
-            print"<th>summaryQuote</th>";
-        print "</tr>";
+
+        print "<select id='companyList' onchange= 'getprice()'>";
+        print "<option value=''>Select a Company</option>";
+
             while ($recordArray = mysqli_fetch_row($results)) {
 
                 //extracting field's values
                 $id = $recordArray[0];
                 $exchange = $recordArray[1];
                 $symbol = $recordArray[2];
-                $name = $recordArray[3];
+                $companyName = $recordArray[3];
                 $lastSale = $recordArray[4];
                 $marketCap = $recordArray[5];
                 $ADRTSO = $recordArray[6];
@@ -61,24 +48,14 @@ print"User's search: ".$searchText;
                 $sector = $recordArray[8];
                 $industry = $recordArray[9];
                 $summaryQuote = $recordArray[10];
-                //string variable that holds all book's information
-                // $collegesdata = $id.",".$collge.",".$address.",".$city.",".$state.",".$zipcode.",".$telephone.",".$website;
 
-            print "<tr>";   
-                print"<td>$exchange</td>";
-                print"<td>$symbol</td>";
-                print"<td>$name</td>";
-                print"<td>$lastSale</td>";
-                print"<td>$marketCap</td>";
-                print"<td>$ADRTSO</td>";
-                print"<td>$IPOyear</td>";
-                print"<td>$sector</td>";
-                print"<td>$industry</td>";
-                print"<td><a href=".$summaryQuote.">$summaryQuote</a></td>";
-            print "</tr>";
+                //send the values as a option
+                
+                print "<option value='".$symbol."'>".$companyName."</option>";
+            
         }
 
-        print "</table>";
+        print "</select>";
     } else {
         print" No record(s) found";
     }
